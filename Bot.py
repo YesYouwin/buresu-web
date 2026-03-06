584adf3ed4da6d29687134609386488fdaa7b936
YesYouwin <garenagaming32@gmail.com>
2026-03-06 21:38:01 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -17,5 +17,60 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
+def convert_to_sgt(time_str):
+    ist = datetime.datetime.strptime(time_str, "%I:%M%p")
+    sgt = ist + datetime.timedelta(hours=2, minutes=30)
+    return sgt.strftime("%I:%M %p")
 
+
+@bot.command(name="game")
+async def scrim(ctx, scrim_type, teams, time, timezone, date, map_name):
+
+    try:
+
+        team1, team2 = teams.split("Vs")
+
+        sgt_time = convert_to_sgt(time)
+
+        embed = discord.Embed(
+            title="🎮 Scrim | Scrim Schedule",
+            color=discord.Color.red()
+        )
+
+        embed.add_field(
+            name="Teams",
+            value=f"{team1} VS {team2}",
+            inline=False
+        )
+
+        embed.add_field(
+            name="Time",
+            value=f"{time} {timezone} / {sgt_time} SGT",
+            inline=False
+        )
+
+        embed.add_field(
+            name="Day/Date",
+            value=date,
+            inline=False
+        )
+
+        embed.add_field(
+            name="Map",
+            value=map_name,
+            inline=False
+        )
+
+        embed.add_field(
+            name="Note",
+            value="The @IGL of the team is responsible for any player's absence. Please inform beforehand.",
+            inline=False
+        )
+
+        await ctx.send(embed=embed)
+
+    except:
+        await ctx.send("Format error. Example:\n!game In-Houses Team1VsTeam2 7:30PM IST 21/10/2026 Split")
+        
 bot.run(TOKEN)
+
