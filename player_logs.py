59a46b619c3181fba35cd32948782e903d042880
YesYouwin <garenagaming32@gmail.com>
2026-03-09 04:42:05 +0530

    Update player_logs.py

--- player_logs.py
+++ player_logs.py
@@ -134,7 +134,7 @@ class PlayerLogs(commands.Cog):
             else discord.Color.red()
         )
 
-        divider = "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+        divider = "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 
         embed = discord.Embed(
             description=f"""
@@ -233,7 +233,7 @@ class PlayerLogs(commands.Cog):
             await interaction.followup.send("❌ No logs found.", ephemeral=True)
             return
 
-        divider = "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+        divider = "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 
         await interaction.followup.send(
             f"📜 Found **{len(rows)}** log(s) for `{search}`", ephemeral=True
