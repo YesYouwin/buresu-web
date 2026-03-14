17ebba5df8a29f02c918b9d3d2c179cbc7273774
YesYouwin <garenagaming32@gmail.com>
2026-03-14 14:42:06 +0530

    app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return "Six seven?"
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
-# HEHEHLKADJALKDJ
